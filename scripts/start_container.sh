9c4761d3a6e8e6bb45b662224a1756067524ab57
Swapnil31Ingale <107902990+Swapnil31Ingale@users.noreply.github.com>
2025-07-21 01:21:19 +0530

    Update start_container.sh
    Correct image name

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull swapnil31ingale/simple-python-flask-app
+docker pull swapnil31ingale/demo-python-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 swapnil31ingale/simple-python-flask-app
+docker run -d -p 5000:5000 swapnil31ingale/demo-python-app
+
